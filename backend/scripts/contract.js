eb0dd4303f812f0aceed59acaa8779df46f8bfcf
Maves1 <seva21122@gmail.com>
2023-12-04 19:24:51 +0300

    beautify contract.js code

--- backend/scripts/contract.js
+++ backend/scripts/contract.js
@@ -2,12 +2,20 @@
 const Web3 = require("web3");
 var fs = require('fs');
 
+var debugLogsEnabled = false;
+
+// Connecting to a local blockchain for testing purposes
 const web3 = new Web3.Web3("http://localhost:8545");
+
+// Contract ABI
 var jsonFile = "scripts/TRC721NFT.json";
 var parsed = JSON.parse(fs.readFileSync(jsonFile));
 var abi = parsed.abi;
 
+// Contract address on the blockchain
 const address = "0x6576e89dd26fc2c3709fc0ec4f2ebe010c313457";
+
+// Creating the contract object
 const nftContract = new web3.eth.Contract(abi, address);
 
 async function mintNFT(telegram_alias) {
@@ -15,38 +23,25 @@ async function mintNFT(telegram_alias) {
         const accounts = await web3.eth.getAccounts();
         const fromAddress = accounts[0]; // Using the first account in the list
 
-        console.log('Account:', fromAddress);
-
-        // // Replace "fromAddress" with the appropriate Ethereum address if necessary
-        // const tx = await nftContract.methods.mintNFT(fromAddress, telegram_alias).send({ from: fromAddress });
-
         const gas = await nftContract.methods.mintNFT(fromAddress, telegram_alias).estimateGas({ from: fromAddress });
-
-        console.log('Estimated gas:', gas);
-
-        // Send the transaction
         const response = await nftContract.methods.mintNFT(fromAddress, telegram_alias).send({ from: fromAddress, gas });
 
         console.log('Transaction successful:', response);
 
-        const balance = await nftContract.methods.balanceOf(fromAddress).call();
+        if (debugLogsEnabled) {
+            const balance = await nftContract.methods.balanceOf(fromAddress).call();
 
-        if (balance === 0) {
-            console.log('User does not own any NFTs');
-            return;
+            if (balance === 0) {
+                console.log('User does not own any NFTs');
+                return;
+            }
+
+            // Checking all nfts owned by the current user
+            for (let i = 0; i < balance; i++) {
+                let tokenId = await nftContract.methods.tokenOfOwnerByIndex(fromAddress, i).call();
+                console.log(`Token ID at index ${i}:`, tokenId);
+            }
         }
-
-        // Retrieve the first NFT's token ID owned by the user
-        const tokenId = await nftContract.methods.tokenOfOwnerByIndex(fromAddress, 0).call();
-        console.log('Token ID:', tokenId);
-
-        // If you need to retrieve all token IDs, loop through the balance
-        for (let i = 0; i < balance; i++) {
-            let tokenId = await nftContract.methods.tokenOfOwnerByIndex(fromAddress, i).call();
-            console.log(`Token ID at index ${i}:`, tokenId);
-        }
-
-
     } catch (error) {
         console.error('Error in transaction:', error);
     }
